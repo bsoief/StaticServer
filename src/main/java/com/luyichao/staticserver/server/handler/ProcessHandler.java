94034a4627b8a3922cc2e69323d513d4ae9376d9
yichao.lu <136070559@qq.com>
2018-02-14 14:19:27 +0800

    头部添加长度

--- src/main/java/com/luyichao/staticserver/server/handler/ProcessHandler.java
+++ src/main/java/com/luyichao/staticserver/server/handler/ProcessHandler.java
@@ -1,28 +1,38 @@
 package com.luyichao.staticserver.server.handler;
 
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.util.AsciiString;
 
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class ProcessHandler extends SimpleChannelInboundHandler<FullHttpRequest>{
 
+    private static final AsciiString CONTENT_TYPE = new AsciiString("Content-Type");
+    private static final AsciiString CONTENT_LENGTH = new AsciiString("Content-Length");
+
     protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
         String uri = request.uri();
         System.out.print("uri : " + uri);
 
         test(ctx);
+    }
 
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
     }
 
     private void test(ChannelHandlerContext ctx) throws Exception{
         FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.OK, Unpooled.wrappedBuffer("OK".getBytes("UTF-8")));
-        ctx.writeAndFlush(response);
+        response.headers().setInt(CONTENT_LENGTH, response.content().readableBytes());
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
     }
 }
